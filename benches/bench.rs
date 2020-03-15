b6249030e3d5cba168f79a8ceead01a8af6d9eee
David Holroyd <dave@badgers-in-foil.co.uk>
2020-03-15 08:53:57 +0000

    Pull more code into the benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,10 +9,31 @@ use criterion::Throughput;
 use std::io::Read;
 use h264_reader::annexb::AnnexBReader;
 use h264_reader::annexb::NalReader;
-use h264_reader::Context;
+use h264_reader::{Context, nal};
+use h264_reader::rbsp::RbspDecoder;
+use h264_reader::nal::NalHandler;
+use h264_reader::nal::NalHeader;
 
-#[derive(Default)]
+struct NullNalHandler {
+}
+impl NalHandler for NullNalHandler {
+    type Ctx = ();
+
+    fn start(&mut self, _ctx: &mut Context<Self::Ctx>, _header: NalHeader) {
+        unimplemented!()
+    }
+
+    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, _buf: &[u8]) {
+        unimplemented!()
+    }
+
+    fn end(&mut self, _ctx: &mut Context<Self::Ctx>) {
+        unimplemented!()
+    }
+}
+
 struct NullNalReader {
+    decoder: RbspDecoder<NullNalHandler>,
     start: u64,
     push: u64,
     end: u64,
@@ -37,7 +58,13 @@ fn h264_reader(c: &mut Criterion) {
     let mut buf = vec![0; size];
     f.read(&mut buf[..]).unwrap();
     let mut ctx = Context::default();
-    let nal_reader = NullNalReader::default();
+    let nal_handler = NullNalHandler {};
+    let nal_reader = NullNalReader {
+        decoder: RbspDecoder::new(nal_handler),
+        start: 0,
+        push: 0,
+        end: 0,
+    };
     let mut annexb_reader = AnnexBReader::new(nal_reader);
     c.bench("parse", Benchmark::new("parse", move |b| {
         b.iter(|| {
