8dd622c64ee4df378ffbe4e947dd6e58fc580cc3
Scott Lamb <slamb@slamb.org>
2021-06-09 04:30:25 -0700

    resurrect bench
    *   fix compilation, which broke with 90935499. Criterion changed
        Throughput::Bytes to take a u64 rather than a u32 as part of the
        0.2 -> 0.3 update.
    *   build on CI so this doesn't break again.
    *   add instructions for generating input data, and .gitignore it.
    *   fix warnings.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,11 +1,19 @@
+//! Benchmark on a large video file.
+//!
+//! Expects a copy of [Big Buck Bunny](https://peach.blender.org/download/):
+//! ```text
+//! $ curl --OL https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_h264.mov
+//! $ ffmpeg -i big_buck_bunny_1080p_h264.mov -c copy big_buck_bunny_1080p.h264
+//! ```
+
 #[macro_use]
 extern crate criterion;
 extern crate h264_reader;
 
 use criterion::Criterion;
 use std::fs::File;
-use criterion::Benchmark;
 use criterion::Throughput;
+use std::convert::TryFrom;
 use std::io::Read;
 use h264_reader::annexb::AnnexBReader;
 use h264_reader::annexb::NalReader;
@@ -33,7 +41,7 @@ impl NalHandler for NullNalHandler {
 }
 
 struct NullNalReader {
-    decoder: RbspDecoder<NullNalHandler>,
+    _decoder: RbspDecoder<NullNalHandler>,
     start: u64,
     push: u64,
     end: u64,
@@ -53,26 +61,29 @@ impl NalReader for NullNalReader {
 }
 
 fn h264_reader(c: &mut Criterion) {
-    let mut f = File::open("big_buck_bunny_1080p_24fps_h264.h264").expect("file not found");
-    let size = f.metadata().unwrap().len() as usize;
-    let mut buf = vec![0; size];
+    let mut f = File::open("big_buck_bunny_1080p.h264").expect("file not found");
+    let len = f.metadata().unwrap().len();
+    let mut buf = vec![0; usize::try_from(len).unwrap()];
     f.read(&mut buf[..]).unwrap();
     let mut ctx = Context::default();
     let nal_handler = NullNalHandler {};
     let nal_reader = NullNalReader {
-        decoder: RbspDecoder::new(nal_handler),
+        _decoder: RbspDecoder::new(nal_handler),
         start: 0,
         push: 0,
         end: 0,
     };
     let mut annexb_reader = AnnexBReader::new(nal_reader);
-    c.bench("parse", Benchmark::new("parse", move |b| {
+
+    let mut group = c.benchmark_group("parse");
+    group.throughput(Throughput::Bytes(len));
+    group.bench_function("parse", move |b| {
         b.iter(|| {
             annexb_reader.start(&mut ctx);
             annexb_reader.push(&mut ctx, &buf[..]);
             annexb_reader.end_units(&mut ctx);
-        } );
-    }).throughput(Throughput::Bytes(size as u32)));
+        })
+    });
 }
 
 criterion_group!(benches, h264_reader);
