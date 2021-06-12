a7ecfe0590a8b7e40650564bbf8b42d42314c113
Scott Lamb <slamb@slamb.org>
2021-06-12 16:25:39 -0700

    bugfix: strip trailing zero bytes in annexb
    Rec. ITU-T H.264 (03/2010) section 7.4.1 says "The last byte of the NAL
    unit shall not be equal to 0x00." There's a test here showing
    NALs ending in cabac_zero_words, but section 7.4.2.10 says "Each
    cabac_zero_word is represented in a NAL unit by the three-byte sequence
    0x000003 (as a result of the constraints on NAL unit contents that
    result in requiring inclusion of an emulation_prevention_three_byte for
    each cabac_zero_word"
    
    I think this explains caee57b, so revert that change.
    
    There also was a problem before where the produced NAL units were
    different based on the boundaries of pushes. I've added a fuzz test of
    the Annex B encoder that tries everything in a single push and multiple
    pushes. It failed quickly before and runs without error overnight now.
    
    I absorbed the TrailingZero state into the StartTwoZero state, which
    means if there's corruption, the parser will recover after the next
    start code, rather than ignoring data forever.

---
+++ fuzz/fuzz_targets/annexb.rs
@@ -0,0 +1,63 @@
+//! Tests the Annex B parser doesn't crash and has consistent output between a
+//! single push call and a pair of push split at each possible byte location.
+
+#![no_main]
+use hex_slice::AsHex;
+use h264_reader::Context;
+use libfuzzer_sys::fuzz_target;
+use std::convert::TryFrom;
+
+/// Encodes the stream as (4-byte length prefix, NAL)*, as commonly seen in AVC files.
+#[derive(Default)]
+struct AvcBuilder {
+    cur: Vec<u8>,
+}
+
+impl h264_reader::annexb::NalReader for AvcBuilder {
+    type Ctx = Vec<u8>;
+
+    fn start(&mut self, _ctx: &mut Context<Self::Ctx>) {
+        assert!(self.cur.is_empty());
+    }
+    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, buf: &[u8]) {
+        self.cur.extend_from_slice(buf);
+    }
+    fn end(&mut self, ctx: &mut Context<Self::Ctx>) {
+        let len = u32::try_from(self.cur.len()).unwrap();
+        ctx.user_context.extend_from_slice(&len.to_be_bytes()[..]);
+        ctx.user_context.extend_from_slice(&self.cur[..]);
+        self.cur.clear();
+    }
+}
+
+fuzz_target!(|data: &[u8]| {
+    // Parse in a single push.
+    let mut single_push_ctx = h264_reader::Context::new(Vec::new());
+    let mut single_push = h264_reader::annexb::AnnexBReader::new(AvcBuilder::default());
+    single_push.start(&mut single_push_ctx);
+    single_push.push(&mut single_push_ctx, data);
+    single_push.end_units(&mut single_push_ctx);
+
+    for i in 0..data.len() {
+        // Parse in a split push.
+        let mut split_push_ctx = h264_reader::Context::new(Vec::new());
+        let mut split_push = h264_reader::annexb::AnnexBReader::new(AvcBuilder::default());
+        split_push.start(&mut split_push_ctx);
+        let (head, tail) = data.split_at(i);
+        split_push.push(&mut split_push_ctx, head);
+        split_push.push(&mut split_push_ctx, &[]); // also ensure empty pushes don't break.
+        split_push.push(&mut split_push_ctx, tail);
+        split_push.end_units(&mut split_push_ctx);
+
+        assert!(single_push_ctx.user_context.as_slice() == split_push_ctx.user_context.as_slice(),
+                "inconsistent output.\n\
+                split point: {}\n\
+                input:       {:02x}\n\
+                single push: {:02x}\n\
+                split push:  {:02x}",
+                i,
+                data.as_hex(),
+                single_push_ctx.user_context.as_hex(),
+                split_push_ctx.user_context.as_hex());
+    }
+});
