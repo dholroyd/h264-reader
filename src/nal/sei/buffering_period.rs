0531ae292d88888f7c2c3b8f28cb0b29238ba92b
Scott Lamb <slamb@slamb.org>
2021-06-16 14:21:17 -0700

    test and fix for SEI buffering period parsing
    This should be using initial_cpb_removal_delay_length_minus1 rather
    than cpb_removal_delay_length_minus1. I've added a test extracted from
    a conformance bitstream. I also reduced the "rightward drift" in
    BufferingPeriod::read while I was here.

--- src/nal/sei/buffering_period.rs
+++ src/nal/sei/buffering_period.rs
@@ -1,6 +1,6 @@
 use super::SeiCompletePayloadReader;
 use std::marker;
-use crate::nal::pps;
+use crate::nal::{sps, pps};
 use crate::rbsp::RbspBitReader;
 use crate::Context;
 use crate::nal::sei::HeaderType;
@@ -24,7 +24,7 @@ impl From<pps::ParamSetIdError> for BufferingPeriodError {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct InitialCpbRemoval {
     initial_cpb_removal_delay: u32,
     initial_cpb_removal_delay_offset: u32,
@@ -41,7 +41,7 @@ fn read_cpb_removal_delay_list(r: &mut RbspBitReader<'_>, count: usize, length: 
     Ok(res)
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct BufferingPeriod {
     nal_hrd_bp: Option<Vec<InitialCpbRemoval>>,
     vcl_hrd_bp: Option<Vec<InitialCpbRemoval>>,
@@ -50,33 +50,20 @@ impl BufferingPeriod {
     fn read<Ctx>(ctx: &Context<Ctx>, buf: &[u8]) -> Result<BufferingPeriod,BufferingPeriodError> {
         let mut r = RbspBitReader::new(buf);
         let seq_parameter_set_id = pps::ParamSetId::from_u32(r.read_ue_named("seq_parameter_set_id")?)?;
-        match ctx.sps_by_id(seq_parameter_set_id) {
-            None => Err(BufferingPeriodError::UndefinedSeqParamSetId(seq_parameter_set_id)),
-            Some(sps) => {
-                let vui = sps.vui_parameters.as_ref();
-                let nal_hrd_bp = if let Some((cpb_removal_delay_length_minus1, nal_cpb_cnt)) = vui
-                    .and_then(|vui_params| vui_params.nal_hrd_parameters.as_ref() )
-                    .map(|nal_hrd_params| (nal_hrd_params.cpb_removal_delay_length_minus1, nal_hrd_params.cpb_specs.len()) )
-                {
-                    Some(read_cpb_removal_delay_list(&mut r, nal_cpb_cnt, cpb_removal_delay_length_minus1+1)?)
-                } else {
-                    None
-                };
-                let vcl_hrd_bp = if let Some((cpb_removal_delay_length_minus1, vcl_cpb_cnt)) = vui
-                    .and_then(|vui_params| vui_params.vcl_hrd_parameters.as_ref() )
-                    .map(|vcl_hrd_params| (vcl_hrd_params.cpb_removal_delay_length_minus1, vcl_hrd_params.cpb_specs.len()) )
-                {
-                    Some(read_cpb_removal_delay_list(&mut r, vcl_cpb_cnt, cpb_removal_delay_length_minus1+1)?)
-                } else {
-                    None
-                };
-
-                Ok(BufferingPeriod {
-                    nal_hrd_bp,
-                    vcl_hrd_bp,
-                })
-            }
-        }
+        let sps = ctx.sps_by_id(seq_parameter_set_id)
+            .ok_or_else(|| BufferingPeriodError::UndefinedSeqParamSetId(seq_parameter_set_id))?;
+        let vui = sps.vui_parameters.as_ref();
+        let mut read = |p: &sps::HrdParameters| read_cpb_removal_delay_list(
+            &mut r,
+            p.cpb_specs.len(),
+            p.initial_cpb_removal_delay_length_minus1 + 1,
+        );
+        let nal_hrd_bp = vui.and_then(|v| v.nal_hrd_parameters.as_ref()).map(&mut read).transpose()?;
+        let vcl_hrd_bp = vui.and_then(|v| v.vcl_hrd_parameters.as_ref()).map(&mut read).transpose()?;
+        Ok(BufferingPeriod {
+            nal_hrd_bp,
+            vcl_hrd_bp,
+        })
     }
 }
 pub struct BufferingPeriodPayloadReader<Ctx> {
@@ -100,5 +87,41 @@ impl<Ctx> SeiCompletePayloadReader for BufferingPeriodPayloadReader<Ctx> {
                 info!("TODO: expose buffering_period {:#?}", buffering_period);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use hex_literal::hex;
+
+    use super::*;
+
+    #[test]
+    fn parse() {
+        // https://standards.iso.org/ittf/PubliclyAvailableStandards/ISO_IEC_14496-4_2004_Amd_6_2005_Bitstreams/
+        // This example taken from CVSEFDFT3_Sony_E.zip.
+        let mut ctx = Context::default();
+        let sps_rbsp = hex!("
+            4d 60 15 8d 8d 28 58 9d 08 00 00 0f a0 00 07 53
+            07 00 00 00 92 7c 00 00 12 4f 80 fb dc 18 00 00
+            0f 42 40 00 07 a1 20 7d ee 07 c6 0c 62 60
+        ");
+        ctx.put_seq_param_set(sps::SeqParameterSet::from_bytes(&sps_rbsp[..]).unwrap());
+
+        let payload = &hex!("d7 e4 00 00 57 e4 00 00 40")[..];
+        assert_eq!(BufferingPeriod::read(&ctx, payload).unwrap(), BufferingPeriod {
+            nal_hrd_bp: Some(vec![
+                InitialCpbRemoval {
+                    initial_cpb_removal_delay: 45_000,
+                    initial_cpb_removal_delay_offset: 0,
+                },
+            ]),
+            vcl_hrd_bp: Some(vec![
+                InitialCpbRemoval {
+                    initial_cpb_removal_delay: 45_000,
+                    initial_cpb_removal_delay_offset: 0,
+                },
+            ]),
+        });
     }
 }
